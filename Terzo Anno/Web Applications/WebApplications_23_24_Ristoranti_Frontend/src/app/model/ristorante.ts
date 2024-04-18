fb027be265dbc6b110faeafe82ce78fba2249690
Rhaegal222 <rhaegal222@gmail.com>
2024-04-18 15:29:13 +0200

    Rimozione file e configurazioni non utilizzati

--- Terzo Anno/Web Applications/WebApplications_23_24_Ristoranti_Frontend/src/app/model/ristorante.ts
+++
@@ -1,6 +0,0 @@
-export interface Ristorante{
-    id:number;
-    nome:string;
-    descrizione:string;
-    ubicazione:string;
-}
--- Terzo Anno/Web Applications/WebApplications_23_24_Ristoranti_Frontend/src/app/model/utente.ts
+++
@@ -1,8 +0,0 @@
-export interface Utente{
-    username:string;
-    password:string;
-}
-
-export interface AuthToken{
-    token:string;
-} 
